fa21b5e77d11b579483592a0df02f63cf4930755
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-17 11:41:11 +0200

    weip

--- src/screens/Blackjack.jsx
+++ src/screens/Blackjack.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import Section from '../components/Section';
 
 
 export default function Blackjack(props) {
   return (
-    <Section
-      path='/'
-      color='#4286f4'
-      heading='Blackjack'
-      content='This is my Blackjack game.'
-    />
+    <div>
+      <Section
+        path='/'
+        color='#4286f4'
+        heading='Blackjack'
+        content='This is my Blackjack game.'
+      />
+      {/* We want links to take us to locations like
+      /blackjack/player
+      /blackjack/dealer */}
+
+      
+    </div>
   );
 }
