bb2f3b7d023d2da3bee14f1388f2a67c01d514df
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-17 11:51:46 +0200

    weip

--- src/screens/Blackjack.jsx
+++ src/screens/Blackjack.jsx
@@ -18,6 +18,15 @@ export default function Blackjack(props) {
 
       <Link to={props.match.url + '/player'}>Player</Link>
       <Link to={props.match.url + '/dealer'}>Dealer</Link>
+
+      <Route
+        path={props.match.path + '/player'}
+        render={props => <h1>Player at {props.location.pathname}</h1>}
+      />
+      <Route
+        path={props.match.path + '/dealer'}
+        render={props => <h1>Dealer at {props.location.pathname}</h1>}
+      />
     </div>
   );
 }
